13b1c1465c653c43599957141f040406d5237009
Emmy Lindgren <emmy.lindgren@hotmail.com>
2022-10-04 10:13:25 +0200

    Added scaffolded API-controllers that we think we need.

---
+++ API/Controllers/CarController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using API.Data;
+using API.Models;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        private readonly Database _context;
+
+        public CarController(Database context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Car
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CarModel>>> GetCars()
+        {
+            return await _context.Cars.ToListAsync();
+        }
+
+        // GET: api/Car/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CarModel>> GetCarModel(int id)
+        {
+            var carModel = await _context.Cars.FindAsync(id);
+
+            if (carModel == null)
+            {
+                return NotFound();
+            }
+
+            return carModel;
+        }
+
+        // PUT: api/Car/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCarModel(int id, CarModel carModel)
+        {
+            if (id != carModel.Ca_Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(carModel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CarModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Car
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<CarModel>> PostCarModel(CarModel carModel)
+        {
+            _context.Cars.Add(carModel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCarModel", new { id = carModel.Ca_Id }, carModel);
+        }
+
+        // DELETE: api/Car/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCarModel(int id)
+        {
+            var carModel = await _context.Cars.FindAsync(id);
+            if (carModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.Cars.Remove(carModel);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CarModelExists(int id)
+        {
+            return _context.Cars.Any(e => e.Ca_Id == id);
+        }
+    }
+}
---
+++ API/Controllers/TripController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using API.Data;
+using API.Models;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TripController : ControllerBase
+    {
+        private readonly Database _context;
+
+        public TripController(Database context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Trip
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TripModel>>> GetTrips()
+        {
+            return await _context.Trips.ToListAsync();
+        }
+
+        // GET: api/Trip/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TripModel>> GetTripModel(int id)
+        {
+            var tripModel = await _context.Trips.FindAsync(id);
+
+            if (tripModel == null)
+            {
+                return NotFound();
+            }
+
+            return tripModel;
+        }
+
+        // PUT: api/Trip/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTripModel(int id, TripModel tripModel)
+        {
+            if (id != tripModel.Tr_Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tripModel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TripModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Trip
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TripModel>> PostTripModel(TripModel tripModel)
+        {
+            _context.Trips.Add(tripModel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTripModel", new { id = tripModel.Tr_Id }, tripModel);
+        }
+
+        // DELETE: api/Trip/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTripModel(int id)
+        {
+            var tripModel = await _context.Trips.FindAsync(id);
+            if (tripModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.Trips.Remove(tripModel);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TripModelExists(int id)
+        {
+            return _context.Trips.Any(e => e.Tr_Id == id);
+        }
+    }
+}
