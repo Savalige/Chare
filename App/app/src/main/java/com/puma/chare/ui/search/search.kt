68e8ba7f94a95788a473c2ffbfef5c7d874a7ebd
ChristofferBillman <christoffer.billman@gmail.com>
2022-10-04 15:54:10 +0200

    feat: added eventlistener to button + getting values from input fields.

--- App/app/src/main/java/com/puma/chare/ui/search/search.kt
+++ App/app/src/main/java/com/puma/chare/ui/search/search.kt
@@ -1,12 +1,15 @@
 package com.puma.chare.ui.search
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.textfield.TextInputEditText
 import com.puma.chare.R
 
 
@@ -17,13 +20,28 @@ class search : Fragment() {
     }
 
     private lateinit var viewModel: SearchViewModel
+    private lateinit var originInput: TextInputEditText
+    private lateinit var destinationInput: TextInputEditText
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Hide the actionbar.
         (activity as AppCompatActivity?)!!.supportActionBar!!.hide()
-        return inflater.inflate(R.layout.fragment_search, container, false)
+
+        // Inflate view.
+        val view = inflater.inflate(com.puma.chare.R.layout.fragment_search, container, false)
+
+        // Get Button from inflated view.
+        val button: Button = view.findViewById(R.id.submitButton)
+
+        // Add eventListener on Button.
+        button.setOnClickListener {
+            onSubmit(view)
+            Log.d("ok", "Hej!")
+        }
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -32,4 +50,15 @@ class search : Fragment() {
         // TODO: Use the ViewModel
     }
 
+    private fun onSubmit(view: View) {
+        originInput = view.findViewById(R.id.origin_input)
+        destinationInput = view.findViewById(R.id.destination_input)
+
+        val origin = originInput.text
+        val destination = destinationInput.text
+
+        // Origin and destination is ready to be used.
+        // Eg. send to next view or to API for processing.
+        Log.d("ok: ", "$origin")
+    }
 }
