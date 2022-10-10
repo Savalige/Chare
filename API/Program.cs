a5d167dfe4b4a56efa5a703191af24fa9dda6fb6
ChristofferBillman <christoffer.billman@gmail.com>
2022-10-10 16:21:47 +0200

    wip: added CORS configuration. Might not be necessary.

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,22 @@
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy(name: MyAllowSpecificOrigins,
+					  policy =>
+					  {
+						  policy.WithOrigins("https://localhost:44413",
+											  "https://localhost:7256",
+											  "https://localhost:5256")
+											  .AllowAnyHeader()
+											  .AllowAnyMethod();
+					  });
+});
 
 // Add services to the container.
 
