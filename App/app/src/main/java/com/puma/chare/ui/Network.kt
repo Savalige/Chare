b59118c3cb53196a560803f560fb189351d8818f
ChristofferBillman <christoffer.billman@gmail.com>
2022-10-10 13:46:36 +0200

    feat: created class Network to handle network requests.

---
+++ App/app/src/main/java/com/puma/chare/ui/Network.kt
@@ -0,0 +1,54 @@
+package com.puma.chare.ui
+
+import android.provider.Settings.Global
+import android.util.Log
+import com.google.gson.Gson
+import kotlinx.coroutines.*
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.URL
+import java.util.stream.Collectors
+import javax.net.ssl.HttpsURLConnection
+
+class Network(url: String) {
+    lateinit var url: URL
+    lateinit var connection: HttpsURLConnection
+
+    init {
+        this.url = URL(url)
+        this.connection = this.url.openConnection() as HttpsURLConnection
+    }
+
+    /**
+     * Performs a GET request.
+     * NOTE: call await on Deferred<String> returned by this function to get value.
+     *       Must be used inside a coroutine.
+     */
+    public fun getAsync(): Deferred<String> = GlobalScope.async {
+        try {
+            // Length of buffer is not known beforehand, so setChuckedStreamingMode(0) is set.
+            connection.setChunkedStreamingMode(0)
+
+            val inputStream: InputStream = connection.inputStream
+
+            // Create BufferedReader from InputStream.
+            val bf: BufferedReader = BufferedReader(InputStreamReader(inputStream))
+
+            val gson: Gson = Gson()
+
+            // Convert BufferedReader to String and return it.
+            return@async bf.lines().collect(Collectors.joining())
+        } finally {
+            connection.disconnect()
+    }
+}
+
+    public fun Post() = GlobalScope.async {
+    }
+
+    public fun Delete() {
+
+    }
+
+}
