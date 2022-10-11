c25cb289444ad2b6c2df097f69962e168d1e20cf
id19vnt <viktorianordkvist@gmail.com>
2022-10-11 10:49:22 +0200

    Updated models and review controller.

--- App/app/src/main/java/com/puma/chare/models/Profile.kt
+++ App/app/src/main/java/com/puma/chare/models/Profile.kt
@@ -5,5 +5,6 @@ import java.util.Date
 data class Profile (var Pr_Id: Int?, var Pr_FirstName: String, var Pr_LastName: String,
                     var Pr_BirthDate: Date, var Pr_Img: String?, var Pr_DriveDistance: Int
                     ,var Pr_DriveNum: Int, var Pr_RideDistance: Int, var Pr_RideNum: Int,
-                    var Pr_Email: String, private var Pr_Password: String, var Pr_Bio: String){
+                    var Pr_Email: String, private var Pr_Password: String, var Pr_Bio: String,
+                    var Pr_ProfileInterest: MutableList<Interest>?){
 }
