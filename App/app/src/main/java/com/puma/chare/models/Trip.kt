1f8abcb3774d7f519f22fd4e68862e28770c8f26
Emmy Lindgren <emmy.lindgren@hotmail.com>
2022-10-11 10:44:41 +0200

    Updated classes to keep intersection information

--- App/app/src/main/java/com/puma/chare/models/Trip.kt
+++ App/app/src/main/java/com/puma/chare/models/Trip.kt
@@ -3,6 +3,9 @@ package com.puma.chare.models
 import java.util.*
 
 data class Trip (var Tr_Id: Int?, var Tr_Date: Date,
-                 var Tr_AvaliableSeats: Int, var Tr_Price: Double, var Tr_Destinations: String){
-
+                 var Tr_AvaliableSeats: Int, var Tr_Price: Double, var Tr_Destinations: String
+                 ,var Tr_Driver: Profile, var Tr_ApprovedPassengers: MutableList<ApprovedPassenger>?
+                 ,var Tr_Requests: MutableList<Request>
+                 ,var Tr_DeclinedProfiles: MutableList<Profile>
+                 ,var Tr_TripPreferences: MutableList<Preference>){
 }
