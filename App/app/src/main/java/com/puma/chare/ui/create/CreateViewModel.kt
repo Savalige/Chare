a74954018a70121a359a6e812ed3094a04a39fb2
ChristofferBillman <christoffer.billman@gmail.com>
2022-10-12 10:46:34 +0200

    wip: Need to merge with master to continue.

--- App/app/src/main/java/com/puma/chare/ui/create/CreateViewModel.kt
+++ App/app/src/main/java/com/puma/chare/ui/create/CreateViewModel.kt
@@ -1,7 +1,11 @@
 package com.puma.chare.ui.create
 
 import androidx.lifecycle.ViewModel
+import java.util.Date
 
 class CreateViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+
+    public fun part1ToViewModel(origin: String, destination: String, dateTime: Date) {
+    }
 }
