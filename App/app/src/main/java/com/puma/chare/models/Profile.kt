03246b7e4d367f8bbe245724476ca44c3f1c4364
ChristofferBillman <christoffer.billman@gmail.com>
2022-10-18 16:13:43 +0200

    wip: logic behind the createUser form. Untested.

--- App/app/src/main/java/com/puma/chare/models/Profile.kt
+++ App/app/src/main/java/com/puma/chare/models/Profile.kt
@@ -3,8 +3,11 @@ package com.puma.chare.models
 import java.util.Date
 
 data class Profile (var pr_Id: Int?, var pr_Firstname: String, var pr_Lastname: String,
-                    var pr_BirthDate: Date, var pr_Img: String?, var pr_DriveDistance: Int
+                    var pr_BirthDate: Date?, var pr_Img: String?, var pr_DriveDistance: Int
                     ,var pr_DriveNum: Int, var pr_RideDistance: Int, var pr_RideNum: Int,
-                    var pr_Email: String, private var pr_Password: String, var pr_Bio: String,
+                    var pr_Email: String, var pr_Password: String, var pr_Bio: String,
                     var pr_ProfileInterest: MutableList<Interest>?){
+    constructor() : this(null,"","",null,null,
+        0,0,0,0,"",
+        "","",null)
 }
--- App/app/src/main/java/com/puma/chare/ui/createUser/CreateUserViewModel.kt
+++ App/app/src/main/java/com/puma/chare/ui/createUser/CreateUserViewModel.kt
@@ -1,7 +1,38 @@
 package com.puma.chare.ui.createUser
 
 import androidx.lifecycle.ViewModel
+import com.puma.chare.models.Car
+import com.puma.chare.models.Profile
+import java.util.Date
 
 class CreateUserViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val profile: Profile = Profile()
+    private lateinit var car: Car
+
+    fun passFragment1DataToViewModel(firstname: String,lastname: String, birthdate: String,
+                                     email: String, password: String){
+        profile.pr_Firstname = firstname
+        profile.pr_Lastname = lastname
+        // TODO: Remove hardocded date.
+        profile.pr_BirthDate = Date()
+        profile.pr_Email = email
+        profile.pr_Password = password
+    }
+    fun passFragment2DataToViewModel(bio: String){
+        profile.pr_Bio = bio
+    }
+    fun passFragment3DataToViewModel(car: Car){
+        this.car = car
+    }
+
+    public fun getProfile(): Profile {
+        return profile
+    }
+    public fun getCar(): Car {
+        return car
+    }
+
+    fun submitProfile() {
+
+    }
 }
