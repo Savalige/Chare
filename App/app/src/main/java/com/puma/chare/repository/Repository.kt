9d1808aa3b217a4da00487592b3365f860c41e1b
ChristofferBillman <christoffer.billman@gmail.com>
2022-10-18 13:29:00 +0200

    feat: wip, commiting to merge master

--- App/app/src/main/java/com/puma/chare/repository/Repository.kt
+++ App/app/src/main/java/com/puma/chare/repository/Repository.kt
@@ -7,4 +7,7 @@ class Repository {
     suspend fun getProfile(id: String): Profile {
         return RetrofitInstance.api.getProfile(id)
     }
+    suspend fun postProfile(profile: Profile) {
+        return RetrofitInstance.api.postProfile(profile)
+    }
 }
